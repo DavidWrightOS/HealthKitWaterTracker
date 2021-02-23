962fc9ae959147a4e9a8bf44dde8d5a6b8fc98c2
David Wright <davidtwright89@gmail.com>
2021-02-22 23:48:14 -0500

    Initial Commit

---
+++ HealthKitWaterTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthKitWaterTracker
+//
+//  Created by David Wright on 2/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
