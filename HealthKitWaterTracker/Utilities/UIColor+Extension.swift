6a3d0317c7a54e847da46ddbbe39fae0f224f386
David Wright <davidtwright89@gmail.com>
2021-02-24 14:58:22 -0500

    Refactored the UIColor extension to use consistent naming and separate out the custom defined colors

--- HealthKitWaterTracker/Utilities/UIColor+Extension.swift
+++ HealthKitWaterTracker/Utilities/UIColor+Extension.swift
@@ -7,19 +7,31 @@
 
 import UIKit
 
+// MARK: - Custom Colors
+
 extension UIColor {
-    
-    static let waterColorScheme: Bool = false
+    private static var customBlue = #colorLiteral(red: 0.2117647059, green: 0.2431372549, blue: 0.337254902, alpha: 1)
+    private static var customBlueSecondary = #colorLiteral(red: 0.2901960784, green: 0.3176470588, blue: 0.4039215686, alpha: 1)
+    private static var customTeal = #colorLiteral(red: 0.1411764706, green: 0.5411764706, blue: 0.6196078431, alpha: 1)
+    private static var customWhite = #colorLiteral(red: 0.8470588235, green: 0.8470588235, blue: 0.8470588235, alpha: 1)
+    private static var customWhiteSecondary = #colorLiteral(red: 0.5571184754, green: 0.5771605372, blue: 0.619569242, alpha: 1)
+}
+
+
+// MARK: - Dynamic Colors
+
+extension UIColor {
+    static let waterColorScheme: Bool = true
     
     static var tabBarItemColor: UIColor { waterColorScheme ? .detailTextColor : .systemGray }
     static var tabBarSelectedItemColor: UIColor { .actionColor }
-    static var tabBarTintColor: UIColor? { waterColorScheme ? .groupedBackgroundColor : nil }
-    static var tabBarBackgroundColor: UIColor { waterColorScheme ? #colorLiteral(red: 0.2117647059, green: 0.2431372549, blue: 0.337254902, alpha: 1) : .secondarySystemGroupedBackground }
+    static var tabBarTintColor: UIColor? { waterColorScheme ? .secondaryBackgroundColor : nil }
+    static var tabBarBackgroundColor: UIColor { waterColorScheme ? .customBlue : .secondarySystemGroupedBackground }
     
-    static var backgroundColor: UIColor { waterColorScheme ? #colorLiteral(red: 0.2117647059, green: 0.2431372549, blue: 0.337254902, alpha: 1) : .systemGroupedBackground }
-    static var groupedBackgroundColor: UIColor { waterColorScheme ? #colorLiteral(red: 0.2738786042, green: 0.3016990721, blue: 0.3874301314, alpha: 1) : .secondarySystemGroupedBackground }
-    static var textColor: UIColor { waterColorScheme ? #colorLiteral(red: 0.8470588235, green: 0.8470588235, blue: 0.8470588235, alpha: 1) : .label }
-    static var detailTextColor: UIColor { waterColorScheme ? #colorLiteral(red: 0.5571184754, green: 0.5771605372, blue: 0.619569242, alpha: 1) : .secondaryLabel }
-    static var actionColor: UIColor { waterColorScheme ? #colorLiteral(red: 0.1411764706, green: 0.5411764706, blue: 0.6196078431, alpha: 1) : .systemBlue }
-    static var switchOnTintColor: UIColor { waterColorScheme ? #colorLiteral(red: 0.1411764706, green: 0.5411764706, blue: 0.6196078431, alpha: 1) : .systemGreen }
+    static var backgroundColor: UIColor { waterColorScheme ? .customBlue : .systemGroupedBackground }
+    static var secondaryBackgroundColor: UIColor { waterColorScheme ? .customBlueSecondary : .secondarySystemGroupedBackground }
+    static var textColor: UIColor { waterColorScheme ? .customWhite : .label }
+    static var detailTextColor: UIColor { waterColorScheme ? .customWhiteSecondary : .secondaryLabel }
+    static var actionColor: UIColor { waterColorScheme ? .customTeal : .systemBlue }
+    static var switchOnTintColor: UIColor { waterColorScheme ? .customTeal : .systemGreen }
 }
