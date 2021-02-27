487bb93819b0aeacad98373e2ba95c896f09bbed
David Wright <davidtwright89@gmail.com>
2021-02-26 23:21:43 -0500

    Added a SettingsCell

---
+++ HealthKitWaterTracker/Views/Cells/SettingsCell.swift
@@ -0,0 +1,92 @@
+//
+//  SettingsCell.swift
+//  HealthKitWaterTracker
+//
+//  Created by David Wright on 2/26/21.
+//
+
+import UIKit
+
+class SettingsCell: UITableViewCell {
+    
+    static let reuseIdentifier = "SettingsCellReuseIdentifier"
+    
+    // MARK: - Properties
+    
+    var setting: SettingModel? {
+        didSet {
+            guard let setting = setting else { return }
+            label.text = setting.displayName
+            switchControl.isOn = setting.value
+        }
+    }
+    
+    let label: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .body)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var switchControl: UISwitch = {
+        let switchControl = UISwitch()
+        switchControl.translatesAutoresizingMaskIntoConstraints = false
+        switchControl.addTarget(self, action: #selector(switchDidChange), for: .valueChanged)
+        return switchControl
+    }()
+    
+    // MARK: - Initializers
+    
+    convenience init() {
+        self.init(style: .default, reuseIdentifier: SettingsCell.reuseIdentifier)
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .default, reuseIdentifier: reuseIdentifier)
+        
+        selectionStyle = .none
+        
+        contentView.addSubview(label)
+        label.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+        label.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 20).isActive = true
+        
+        contentView.addSubview(switchControl)
+        switchControl.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+        switchControl.leftAnchor.constraint(equalTo: label.rightAnchor, constant: 8).isActive = true
+        switchControl.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -20).isActive = true
+        
+        configureColorScheme()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Helpers
+    
+    private func configureColorScheme() {
+        backgroundColor = .secondaryBackgroundColor
+        textLabel?.textColor = .textColor
+        detailTextLabel?.textColor = .detailTextColor
+        
+        switchControl.onTintColor = .switchOnTintColor
+        switchControl.thumbTintColor = .switchThumbColor
+    }
+    
+    // MARK: - Selectors
+    
+    @objc private func switchDidChange(sender: UISwitch) {
+        setting?.value = sender.isOn
+    }
+}
+
+
+// MARK: - SettingsTracking
+
+extension SettingsCell: SettingsTracking {
+    func healthIntegrationIsEnabledChanged() {}
+    
+    func waterColorSchemeChanged() {
+        configureColorScheme()
+    }
+}
